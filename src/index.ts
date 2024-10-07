215f9634fc53234913472ce7794b65bc13789323
Steve Popoola <stevepop@gmail.com>
2024-10-07 17:35:22 +0100

    Day 1 - Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Task } from './task';
+
+let tasks: Task[] = [
+  { id: 1, title: 'Learn TypeScript', completed: false },
+  { id: 2, title: 'Build a Task Manager app', completed: false }
+];
+
+console.log(tasks);
